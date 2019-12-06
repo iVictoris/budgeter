2234ce172f926f7a9be9d8b0311cab98a21a9dca
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 17:21:10 -0500

    import Transaction component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import TransactionsList from "./components/Transaction/TransactionsList";
+import Transaction from './components/Transaction/Transaction'
 
 function App() {
-
-
   return (
     <div className="App">
-      <section className="Transactions">
-        <header className="Transactions-heading">
-          <section className="Transactions-header">ID</section>
-          <section className="Transactions-header">Description</section>
-          <section className="Transactions-header">Debits</section>
-          <section className="Transactions-header">Credits</section>
-          <section className="Transactions-header">Category</section>
-        </header>
-        <TransactionsList />
-      </section>
+      <Transaction />
     </div>
   );
 }
