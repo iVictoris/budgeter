630e33d45e06ca79263b6888b210ce9d1261e850
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 18:59:38 -0500

    move LookupForm into it's own component

--- src/App.js
+++ src/App.js
@@ -12,27 +12,7 @@ function App() {
   );
 }
 
-const LookupForm = () => {
-  return (
-    <Form>
-      <label htmlFor="update-id">Lookup ID</label>
-      <Field id="update-id" name="id" type="text" />
-      <button type="submit">Find Transaction</button>
-    </Form>
-  );
-};
 
-const EnhancedLookupForm = withFormik({
-  mapPropsToValues({ id }) {
-    return {
-      id
-    };
-  },
-
-  handleSubmit(values, formikBag) {
-    return;
-  }
-})(LookupForm);
 
 const UpdateForm = ({ id }) => {
   // query for transaction somewhere...
