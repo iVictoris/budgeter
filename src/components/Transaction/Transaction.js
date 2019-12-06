81f98f0249d1b28c1321d0479d6eb93cdf8c30e2
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 17:01:22 -0500

    create transaction component

---
+++ src/components/Transaction/Transaction.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const formatMoney = (amount) => {
+  const dollars = Number.parseFloat(amount).toFixed(2)
+
+
+  return `$${dollars}`
+}
+
+const Transaction = ({transactionID, description, debit, credit, category}) => {
+  return (
+    <section className="Transaction">
+      <section className="Transaction-key">{transactionID}</section>
+      <section className="Transaction-description">{description}</section>
+      <section className="Transaction-debit">{formatMoney(debit || "0")}</section>
+      <section className="Transaction-credit">{formatMoney(credit || "0")}</section>
+      <section className="Transaction-category">{category}</section>
+    </section>
+  );
+};
+
+export default Transaction;
