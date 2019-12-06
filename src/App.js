70ae0af85bafe0fd88a424ac3b32a0571d4e36f1
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 17:01:56 -0500

    add TransactionsList to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+import TransactionsList from "./components/Transaction/TransactionsList";
 
 function App() {
+
+
   return (
     <div className="App">
-      <section className='Transactions'>
-        <header className='Transactions-heading'>
+      <section className="Transactions">
+        <header className="Transactions-heading">
           <section className="Transactions-header">ID</section>
           <section className="Transactions-header">Description</section>
           <section className="Transactions-header">Debits</section>
           <section className="Transactions-header">Credits</section>
           <section className="Transactions-header">Category</section>
         </header>
+        <TransactionsList />
       </section>
     </div>
   );
