6e9c76c8d77844a8ed46179cbf2bee0ed73a5b72
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 18:58:20 -0500

    move control panel into it's own component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
 import TransactionTable from "./components/Transaction/TransactionTable";
-import {Form, Field, withFormik} from 'formik'
+import { Form, Field, withFormik } from "formik";
 
 function App() {
   return (
     <div className="App">
       <TransactionTable />
 
-      <section className="Control-Panel">
-        <header className="Control-Panel-header">
-          <h2>Admin Panel</h2>
-        </header>
-
-        <section className="Control-Panel-actions">
-          <section className="Control-Panel-add-transaction">
-            <section className="Control-Panel-add-form-container">
-              <EnhancedAddForm />
-            </section>
-          </section>
-          <section className="Control-Panel-update-transaction"></section>
-          <section className="Control-Panel-delete-transaction"></section>
-        </section>
-      </section>
+      
     </div>
   );
 }
@@ -30,29 +16,98 @@ const AddForm = () => {
   return (
     <Form>
       <label htmlFor="add-description">Description: </label>
-      <Field id='add-description'name='description' type='text' />
+      <Field id="add-description" name="description" type="text" />
 
       <label htmlFor="add-debit">Debit: </label>
-      <Field id='add-debit' name='debit' type='text' />
-      
+      <Field id="add-debit" name="debit" type="text" />
+
       <label htmlFor="add-credit">Credit</label>
-      <Field id='add-credit' name='credit' type='text' />
+      <Field id="add-credit" name="credit" type="text" />
 
       <label htmlFor="add-category">Category: </label>
-      <Field id='add-category' name='category' type='text' />
+      <Field id="add-category" name="category" type="text" />
+
+      <button type="submit">Add Transaction</button>
     </Form>
-  )
-}
+  );
+};
 
 const EnhancedAddForm = withFormik({
-  mapPropsToValues({description, debit, credit, category}) {
-    return {description, debit, credit, category}
+  mapPropsToValues({ description, debit, credit, category }) {
+    return { description, debit, credit, category };
   },
   handleSubmit(values, formikBag) {
     // do something
-    console.log('Submit!')
+    console.log("Submit!");
     return;
   }
 })(AddForm);
+
+const LookupForm = () => {
+  return (
+    <Form>
+      <label htmlFor="update-id">Lookup ID</label>
+      <Field id="update-id" name="id" type="text" />
+      <button type="submit">Find Transaction</button>
+    </Form>
+  );
+};
+
+const EnhancedLookupForm = withFormik({
+  mapPropsToValues({ id }) {
+    return {
+      id
+    };
+  },
+
+  handleSubmit(values, formikBag) {
+    return;
+  }
+})(LookupForm);
+
+const UpdateForm = ({ id }) => {
+  // query for transaction somewhere...
+
+  const transaction = {
+    transactionID: 3,
+    description: "Chipotle",
+    credit: 12.24,
+    category: "Food",
+    debit: 0
+  };
+
+  const {transactionID, description, credit, category, debit} = transaction
+
+  return (
+    <Form>
+      <label htmlFor="update-id">ID</label>
+      <Field id="update-id" name="id" type="text" value={transactionID} disabled />
+
+      <label htmlFor="update-description">Description: </label>
+      <Field id="update-description" name="description" type="text" value={description}/>
+
+      <label htmlFor="update-debit">Debit: </label>
+      <Field id="update-debit" name="debit" type="text" value={debit}/>
+
+      <label htmlFor="update-credit">Credit</label>
+      <Field id="update-credit" name="credit" type="text" value={credit}/>
+
+      <label htmlFor="update-category">Category: </label>
+      <Field id="update-category" name="category" type="text" value={category}/>
+
+      <button type="submit">Update Transaction</button>
+    </Form>
+  );
+};
+
+const EnhancedUpdateForm = withFormik({
+  mapPropsToValues({ id, description, debit, credit, category }) {
+    return { id, description, debit, credit, category };
+  },
+
+  handleSubmit(values, formikBag) {
+    return;
+  }
+})(UpdateForm);
 
 export default App;
