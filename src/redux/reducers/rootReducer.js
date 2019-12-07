bdf02bd74b8d02c3d53a7d4eb436f36890b7e5ab
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 00:16:37 -0500

    add new property in initiaState and new reducer function

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import {
   ADD_TRANSACTION,
   UPDATE_TRANSACTION,
-  DELETE_TRANSACTION
+  DELETE_TRANSACTION,
+  LOOKUP_TRANSACTION
 } from "../actions/actions";
 import {} from "../actions/actionCreators";
 
@@ -35,13 +36,13 @@ const initialState = {
       category: "Food",
       voided: false
     }
-  }
+  },
+  activeTransaction: null
 };
 
 const transactions = (state = {}, action) => {
   switch (action.type) {
     case ADD_TRANSACTION:
-      console.log(action);
       return { ...state, [action.payload.id]: {...action.payload} };
     case UPDATE_TRANSACTION:
         return {...state, [action.payload.id] : action.payload};
@@ -52,9 +53,19 @@ const transactions = (state = {}, action) => {
   }
 };
 
+const lookups = (state = {}, action) => {
+  switch (action.type) {
+    case LOOKUP_TRANSACTION:
+      return {...state, activeTransaction: state[action.payload]}
+    default:
+      return state;
+  }
+}
+
 const rootReducer = (state = initialState, action) => {
   return {
-    transactions: transactions(state.transactions, action)
+    transactions: transactions(state.transactions, action),
+    lookups: lookups(state, action)
   };
 };
 
