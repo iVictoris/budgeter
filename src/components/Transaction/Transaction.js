627e8611bfdb9e92cc756b1c64a72298418c6f37
Victor Tran <victor.n.tran@outlook.com>
2019-12-06 20:26:14 -0500

    format document with prettier

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,19 +1,27 @@
 import React from "react";
 
-const formatMoney = (amount) => {
-  const dollars = Number.parseFloat(amount).toFixed(2)
+const formatMoney = amount => {
+  const dollars = Number.parseFloat(amount).toFixed(2);
+  return `$${dollars}`;
+};
 
-
-  return `$${dollars}`
-}
-
-const Transaction = ({transactionID, description, debit, credit, category}) => {
+const Transaction = ({
+  transactionID,
+  description,
+  debit,
+  credit,
+  category
+}) => {
   return (
     <section className="Transaction">
       <section className="Transaction-key">{transactionID}</section>
       <section className="Transaction-description">{description}</section>
-      <section className="Transaction-debit">{formatMoney(debit || "0")}</section>
-      <section className="Transaction-credit">{formatMoney(credit || "0")}</section>
+      <section className="Transaction-debit">
+        {formatMoney(debit || "0")}
+      </section>
+      <section className="Transaction-credit">
+        {formatMoney(credit || "0")}
+      </section>
       <section className="Transaction-category">{category}</section>
     </section>
   );
