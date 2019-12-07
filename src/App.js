997bd18b56b59146ef3d8fd89c3efc1676dab30a
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 13:12:37 -0500

    Remove modal from app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import TransactionTable from "./components/Transaction/TransactionTable";
 import ControlPanel from "./components/Control-Panel/ControlPanel";
-import ModalContainer from "./components/ui/ModalContainer";
-import Modal from "./components/ui/Modal";
+// import ModalContainer from "./components/ui/ModalContainer";
+// import SuccessModal from './components/Modal/SuccessModal';
 
 function App() {
   return (
     <div className="App">
       <TransactionTable />
       <ControlPanel />
-      <ModalContainer defaultModalState={true}> {/* This will be the success modal */}
-        <Modal type={'success'}>
-
-        </Modal>
-      </ModalContainer>
+      {/* <ModalContainer defaultModalState={true}>
+        <SuccessModal text={'Sucess! Transaction has been created.'}/>
+      </ModalContainer> */}
     </div>
   );
 }
