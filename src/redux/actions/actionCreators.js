8704c156a40947f4b3d60db105ff39cb6f2d4da0
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 00:15:29 -0500

    recreate lookup action creator

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,13 +1,21 @@
 import {
   ADD_TRANSACTION,
   UPDATE_TRANSACTION,
-  DELETE_TRANSACTION
+  DELETE_TRANSACTION,
+  LOOKUP_TRANSACTION
 } from "./actions";
 
 export const addTransaction = (transaction) => {
   return {
     type: ADD_TRANSACTION,
     payload: transaction
+  }
+}
+
+export const lookupTransaction = (id) => {
+  return {
+    type: LOOKUP_TRANSACTION,
+    payload: id
   }
 }
 
