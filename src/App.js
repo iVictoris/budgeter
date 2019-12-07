f7aa4be4a0af2041636cb487c7857772a7284cd9
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 11:28:12 -0500

    test out modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import TransactionTable from "./components/Transaction/TransactionTable";
 import ControlPanel from "./components/Control-Panel/ControlPanel";
+import ModalContainer from "./components/ui/ModalContainer";
+import Modal from "./components/ui/Modal";
 
 function App() {
   return (
     <div className="App">
       <TransactionTable />
       <ControlPanel />
+      <ModalContainer defaultModalState={true}> {/* This will be the success modal */}
+        <Modal type={'success'}>
+
+        </Modal>
+      </ModalContainer>
     </div>
   );
 }
