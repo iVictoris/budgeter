c3e1643fcc3d8b8efc38565d0687fd4ade9b3ac9
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 00:45:22 -0500

    use Yup for validation, connect with redux and use lookupTransaction action creator to look up a transaction

--- src/components/Control-Panel/LookupForm.js
+++ src/components/Control-Panel/LookupForm.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Form, Field, withFormik } from "formik";
+import React from "react";
+import { connect } from "react-redux";
 
-const LookupForm = () => {
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
+
+import { lookupTransaction } from "../../redux/actions/actionCreators";
+
+const LookupForm = ({ errors, touched }) => {
   return (
     <Form>
       <label htmlFor="update-id">Lookup ID</label>
       <Field id="update-id" name="id" type="text" />
+      {touched.id && errors.id && <p className="error">{errors.id}</p>}
       <button type="submit">Find Transaction</button>
     </Form>
   );
 };
 
 const EnhancedLookupForm = withFormik({
-  mapPropsToValues({ id }) {
+  mapPropsToValues({ id = "" }) {
     return {
       id
     };
   },
+  validationSchema: Yup.object().shape({
+    id: Yup.string().required("You must enter an ID to look up a transaction")
+  }),
 
-  handleSubmit(values, formikBag) {
+  handleSubmit({ id }, { props: { lookupTransaction } }) {
+    lookupTransaction(id);
     return;
   }
 })(LookupForm);
 
-export default EnhancedLookupForm
+export default connect(null, { lookupTransaction })(EnhancedLookupForm);
