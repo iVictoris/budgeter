1da08bf1aedf283844884bcb00c6a82f7af24bfc
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 19:04:12 -0500

    make single component of each list

--- src/App.js
+++ src/App.js
@@ -15,14 +15,22 @@ function App() {
       <section className="CashFlowStatement">
         <section className="IncomeList">
           <section className="Income"></section>
+          <section className="Income"></section>
+          <section className="Income"></section>
         </section>
         <section className="ExpenseList">
+          <section className="Expense"></section>
+          <section className="Expense"></section>
           <section className="Expense"></section>
         </section>
         <section className="AssetList">
           <section className="Asset"></section>
+          <section className="Asset"></section>
+          <section className="Asset"></section>
         </section>
         <section className="LiabilityList">
+          <section className="Liability"></section>
+          <section className="Liability"></section>
           <section className="Liability"></section>
         </section>
       </section>
