ee75a4750dfe42383845658d5fef33ed62d43827
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 19:46:39 -0500

    remove unnecessary components from displaying/rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import TransactionTable from "./components/Transaction/TransactionTable";
-import ControlPanel from "./components/Control-Panel/ControlPanel";
+// import TransactionTable from "./components/Transaction/TransactionTable";
+// import ControlPanel from "./components/Control-Panel/ControlPanel";
 // import ModalContainer from "./components/ui/ModalContainer";
 // import SuccessModal from './components/Modal/SuccessModal';
 
 function App() {
   return (
     <div className="App">
-      <TransactionTable />
-      <ControlPanel />
+      {/* <TransactionTable /> */}
+      {/* <ControlPanel /> */}
       {/* <ModalContainer defaultModalState={true}>
         <SuccessModal text={'Sucess! Transaction has been created.'}/>
       </ModalContainer> */}
