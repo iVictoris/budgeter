fd0ffc26f576abf8973b592cbd6516ec03b5fd70
Victor Tran <victor.n.tran@outlook.com>
2019-12-07 19:09:49 -0500

    create total component

--- src/App.js
+++ src/App.js
@@ -17,21 +17,25 @@ function App() {
           <section className="Income"></section>
           <section className="Income"></section>
           <section className="Income"></section>
+          <section className="Total"></section>
         </section>
         <section className="ExpenseList">
           <section className="Expense"></section>
           <section className="Expense"></section>
           <section className="Expense"></section>
+          <section className="Total"></section>
         </section>
         <section className="AssetList">
           <section className="Asset"></section>
           <section className="Asset"></section>
           <section className="Asset"></section>
+          <section className="Total"></section>
         </section>
         <section className="LiabilityList">
           <section className="Liability"></section>
           <section className="Liability"></section>
           <section className="Liability"></section>
+          <section className="Total"></section>
         </section>
       </section>
     </div>
